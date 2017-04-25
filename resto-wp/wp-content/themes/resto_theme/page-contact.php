10e860c5b5028193b6b96dc348ce583c3d5bd384
Bastien <b.setoain@gmail.com>
2017-04-25 16:45:36 +0200

    contact page

--- resto-wp/wp-content/themes/resto_theme/page-contact.php
+++ resto-wp/wp-content/themes/resto_theme/page-contact.php
@@ -1,45 +1,13 @@
 <?php
 
-wp_head(); 
+wp_head();
 
-$context = Timber::get_context();
+$context = timber::get_context();
 
-$plats_posts = array(
-        'post_type' => 'plats_posts',
-        'posts_per_page' => 8
-);
+$post = new TimberPost();
 
-$context['plats'] = Timber::get_posts($plats_posts);
-
-
-
-$blog_posts = array(
-        'post_type' => 'post'
-);
-
-$context['articles'] = Timber::get_posts($blog_posts);
-
-
-
-$plats_posts_featured = array(
-        'post_type' => 'plats_posts',
-        'meta_query'	=> array(
-		'relation'		=> 'AND',
-		array(
-			'key'	  	=> 'featured',
-			'value'	  	=> '1',
-			'compare' 	=> '=',
-		),
-	),
-);
-
-$context['featured'] = Timber::get_posts($plats_posts_featured);
-
-
-
-//var_dump($context);
+$context['post'] = $post;
 
 Timber::render('page-contact.twig', $context);
 
-//var_dump($context['logo']);
-
+//var_dump($context['post']);
