1dbc6032b1de7da30a0fef477d3f71028890bcc2
Bastien <b.setoain@gmail.com>
2017-04-26 10:34:57 +0200

    gulp install

---
+++ resto-wp/wp-content/themes/resto_theme/gulpfile.js
@@ -0,0 +1,26 @@
+// Requis
+var gulp = require('gulp');
+
+// Include plugins
+var plugins = require('gulp-load-plugins')(); // tous les plugins de package.json
+
+// Variables de chemins
+var source = './src'; // dossier de travail
+var destination = './dist'; // dossier à livrer
+
+
+
+'use strict';
+ 
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+ 
+gulp.task('sass', function () {
+  return gulp.src('./sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./css'));
+});
+ 
+gulp.task('sass:watch', function () {
+  gulp.watch('./sass/**/*.scss', ['sass']);
+});
